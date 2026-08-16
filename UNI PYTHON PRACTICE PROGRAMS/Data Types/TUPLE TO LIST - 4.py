9350914106af60820e8409f40c61d02abe6ce04a
MONISH L <monish.l01003@gmail.com>
2026-08-16 23:42:25 +0530

    Remove old practice program data type files from main

--- UNI PYTHON PRACTICE PROGRAMS/Data Types/TUPLE TO LIST - 4.py
+++
@@ -1,4 +0,0 @@
-tuple = ("tuple", 67, 3.14, True, ["list"], {"set"}, {"dict" : "Dictionary"})
-list = list(tuple)
-print(f"\nThe Tuple is: {tuple}\nData Type before conversion: {type(tuple)}")
-print(f"\nData Type after conversion: {type(list)}\nThe List is: {list}")
